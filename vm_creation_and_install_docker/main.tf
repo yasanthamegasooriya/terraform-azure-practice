9b1ab835f34bb254e3a1bbeec3a309f94944f3ad
Yasantha Megasooriya <yasantha1995@gmail.com>
2022-07-07 13:06:30 +0530

    edit gitignore file

--- vm_creation_and_install_docker/main.tf
+++ vm_creation_and_install_docker/main.tf
@@ -120,6 +120,20 @@ resource "azurerm_network_security_rule" "vm-nsg-rule-2" {
   resource_group_name         = azurerm_resource_group.rgtf.name
   network_security_group_name = azurerm_network_security_group.vm-nsg.name
 }
+
+resource "azurerm_network_security_rule" "vm-nsg-rule-3" {
+  name                        = "${var.env_prefix}-nsr-3"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5000"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rgtf.name
+  network_security_group_name = azurerm_network_security_group.vm-nsg.name
+}
 /*
 data "template_file" "linux-vm-docker-setup" {
   template = file("docker_install.sh")
